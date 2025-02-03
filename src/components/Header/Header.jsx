086cbd622de5c11b44ee4298db34077c36675447
bayansuluu <bayanamanapbaeva@gmail.com>
2025-02-03 19:47:42 +0600

    add header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,1 +1,36 @@
+import React from 'react'
+import scss from './Header.module.scss'
 import { headerLinks } from "../../constants/Header";
+import logo from '../../assets/Header/logo.svg'
+import phone from '../../assets/Header/phone.svg'
+import chat from '../../assets/Header/chat.svg'
+
+function Header() {
+
+   const navs = headerLinks.map((el,index) => <a key={index} href={el.link}>{el.title}</a>)
+
+  return (
+    <div className={`${scss.header}`}>
+        <img src={logo} alt="" />
+        <nav>{navs}</nav>
+        <aside>
+            <div>
+            <img src={phone} alt="" />
+            <p>
+                <h1>Call us</h1>
+                <span>(405) 555-0128</span>
+            </p>
+            </div>
+            <div>
+            <img src={chat} alt="" />
+            <p>
+                <h1>Talk to us</h1>
+                <span>hello@createx.com</span>
+            </p>
+            </div>
+        </aside>
+    </div>
+  )
+}
+
+export default Header
