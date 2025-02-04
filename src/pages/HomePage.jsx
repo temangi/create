450a2bc6b59bef15ee1e0f1b5a8f91c54500ce8a
temmirlan <temmirlan@MacBook-Pro--temanbot.local>
2025-02-04 19:42:25 +0600

    add main

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,16 +1,15 @@
-import React from 'react'
-import Service from '../components/Service/Service'
-import Header from '../components/Header/Header'
-
+import React from "react";
+import Service from "../components/Service/Service";
+import Header from "../components/Header/Header";
 
 function HomePage() {
   return (
-   <>
-   <Header/>
-   <Service/>
-   <p>Hello bayana , syimyk</p>
-   </>
-  )
+    <>
+      <Header />
+      <Service />
+      <p>Hello bayana , syimyk</p>
+    </>
+  );
 }
 
-export default HomePage
+export default HomePage;
--- src/pages/News.jsx
+++ src/pages/News.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import Main from '../components/Main/Main'
+import back from "../assets/main/news.svg"
+
+const news = {
+    img : back,
+    title : "Service",
+    desc : "If you are looking for a full-service construction company, look to Createx Construction Bureau. We are doing our best to be a partner for all of your construction needs."
+}
 
 function News() {
   return (
-    <div>News</div>
+   <>
+   <Main {...news} />
+   </>
   )
 }
 
