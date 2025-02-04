50bbf575a5083b30d6fa5b577dca742396e712f7
bayansuluu <bayanamanapbaeva@gmail.com>
2025-02-04 18:54:52 +0600

    add request

---
+++ src/components/Request/Request.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import scss from "./Request.module.scss";
+import bg from "../../assets/request/backgrnd.svg";
+import { arr } from "../../constants/Request";
+
+function Request() {
+  let inputs = arr.map((el, index) => (
+    <div key={index} className={scss.inputs}>
+      {" "}
+      <label htmlFor="">{el.label}</label>{" "}
+      <input
+        className={scss.inputReq}
+        type="text"
+        placeholder={el.placeholder}
+      />
+    </div>
+  ));
+
+  return (
+    <div className={scss.request}>
+      <img src={bg} alt="" />
+      <main>
+        <h1>A quick way to discuss details</h1>
+        <div> {inputs}</div>
+        <label className={scss.mainLabel} htmlFor="">
+          <input type="checkbox" />I agree to receive communications from
+          Createx Construction Bureau.
+        </label>
+        <button>Send request</button>
+      </main>
+    </div>
+  );
+}
+
+export default Request;
