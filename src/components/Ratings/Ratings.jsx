4d5a0dbfaf7998307cd45438585db6d52851fbfe
bayansuluu <bayanamanapbaeva@gmail.com>
2025-02-06 00:45:17 +0600

    add ratings

---
+++ src/components/Ratings/Ratings.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import scss from "./Ratings.module.scss";
+import { people } from "../../constants/Ratings";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+function Ratings() {
+  const settings = {
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1000,
+    arrows: false,
+    adaptiveHeight: true,
+  };
+
+  return (
+    <div className={scss.ratings}>
+      <h1>What our clients are saying</h1>
+      <Slider {...settings}>
+        {people.map((el, index) => (
+          <div key={index} className={scss.eachRating}>
+            <img src={el.img} alt="Client" />
+            <p>{el.text}</p>
+            <div>
+              <h1>{el.name}</h1>
+              <span>{el.position}</span>
+            </div>
+          </div>
+        ))}
+      </Slider>
+    </div>
+  );
+}
+
+export default Ratings;
---
+++ src/constants/Ratings.js
@@ -0,0 +1,32 @@
+import player456 from "../assets/ratings/player456.jfif";
+import player333 from "../assets/ratings/player333.jfif";
+import tanos from "../assets/ratings/tanos.jfif";
+import player001 from "../assets/ratings/player001.pjpeg";
+
+export const people = [
+  {
+    img: player456,
+    text: "Ipsum aute sunt aliquip aute et occaecat. Anim minim do cillum eiusmod enim. Consectetur magna cillum consequat minim  laboris cillum laboris voluptate minim proident exercitation ullamco. ",
+    name: "Seong Gi-hun",
+    position: "Player 456, ex-winner",
+  },
+
+  {
+    img: player001,
+    text: "Ipsum aute sunt aliquip aute et occaecat. Anim minim do cillum eiusmod enim. Consectetur magna cillum consequat minim  laboris cillum laboris voluptate minim proident exercitation ullamco. ",
+    name: "Hwang In-ho",
+    position: "Player 001, Frontman",
+  },
+  {
+    img: tanos,
+    text: "Ipsum aute sunt aliquip aute et occaecat. Anim minim do cillum eiusmod enim. Consectetur magna cillum consequat minim  laboris cillum laboris voluptate minim proident exercitation ullamco. ",
+    name: "Tanos",
+    position: "Player 230",
+  },
+  {
+    img: player333,
+    text: "Ipsum aute sunt aliquip aute et occaecat. Anim minim do cillum eiusmod enim. Consectetur magna cillum consequat minim  laboris cillum laboris voluptate minim proident exercitation ullamco. ",
+    name: "Lee Myung-gi",
+    position: "Player 333",
+  },
+];
