ca2f86ddea27ef179ab3c35864ac3bdaeb663488
temmirlan <temmirlan@MacBook-Pro--temanbot.local>
2025-02-09 19:35:58 +0600

    fix header footer

--- src/App.jsx
+++ src/App.jsx
@@ -7,13 +7,14 @@ import ServiceSecond from './pages/Service.2'
 import News from './pages/News'
 import './App.css'
 import {BrowserRouter,Routes,Route} from "react-router-dom"
-import Header from './components/Header/Header'
+import ScrollToTop from './ScrollTop'
 
 function App() {
 
   return (
    <>
     <BrowserRouter>
+    <ScrollToTop />
       <Routes>
         <Route path='/' element={<HomePage/>} />
         <Route path='/about' element={<About/>} />
